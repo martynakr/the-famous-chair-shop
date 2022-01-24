8789bd729da71fe13549e405c075a23b6ea7c855
martynakr <90227451+martynakr@users.noreply.github.com>
2022-01-24 17:52:47 +1100

    workign homepage

--- src/services/firestore.js
+++ src/services/firestore.js
@@ -7,9 +7,9 @@ import 'firebase/compat/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBDSNsKK0dmyPXqRyg0TnS5F8188Z5lT2I",
-  authDomain: "the-famous-chair-shop.firebaseapp.com",
-  projectId: "the-famous-chair-shop",
-  storageBucket: "the-famous-chair-shop.appspot.com",
+  authDomain: "the-famous-chair-store-2.firebaseapp.com",
+  projectId: "the-famous-chair-store-2",
+  storageBucket: "the-famous-chair-store-2.appspot.com",
   messagingSenderId: "972245838548",
   appId: "1:972245838548:web:67e57ee2411d801fb22941"
 };
